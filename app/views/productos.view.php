7ed6a050fcfbc8cfa1ef605d761120de122fc1d6
Ernesto Quintana <ernesto.quintana.oliva@gmail.com>
2020-09-23 10:52:15 -0300

    modificando apariencia

--- app/views/productos.view.php
+++ app/views/productos.view.php
@@ -1,34 +1,40 @@
 <?php
 
 
-class ViewProducto{
+class ViewProducto
+{
 
-    function renderPagina($productos){
-        
+    function renderPagina($productos)
+    {
+
         include_once 'templates\header.php';
         include_once 'templates\form.php';
 
         echo "<ul class='list-group mt-5'>";
         foreach ($productos as $producto) {
 
-            echo "<li class='list-group-item'>
-                      $producto->nombre |  $producto->precio | $producto->categoria     
-                      <a class='btn btn-secondary btn-sm' href='detalleProducto/$producto->id_producto'>descripcion</a>
-                      <a class='btn btn-dark btn-sm' href='borrar/$producto->id_producto'>Eliminar</a>
-                  </li>";
+            echo "<li class='listaProductoView'>
+                      $producto->nombre |  $producto->precio | $producto->categoria
+                    <div class= 'cajaBtn'>     
+                      <a class='btnDetalle' href='detalleProducto/$producto->id_producto'>descripcion</a>
+                      <a class='btnBorrar' href='borrar/$producto->id_producto'>Eliminar</a>
+                    </div>                  
+                      </li>";
         }
         echo "</ul>";
-       
+
         include_once 'templates\footer.php';
     }
-    
-    function ShowHomeLocation(){
-        header("Location: ".BASE_URL."home");
+
+    function ShowHomeLocation()
+    {
+        header("Location: " . BASE_URL . "home");
     }
-    
-    function renderDetalleProducto($producto){
+
+    function renderDetalleProducto($producto)
+    {
         include_once 'templates\header.php';
-        
+
         echo "<ul class='list-group mt-5'>";
         echo "<li class='list-group-item'>
         $producto->nombre |  $producto->categoria  
@@ -36,10 +42,11 @@ class ViewProducto{
         echo "<li class='list-group-item'> $producto->descripcion | $producto->precio </li>";
         echo "</ul>";
     }
-    
-    function renderProductosByCategoria($productosCategoria){    
+
+    function renderProductosByCategoria($productosCategoria)
+    {
         include_once 'templates\header.php';
-        
+
         echo "<ul class='list-group mt-5'>";
         foreach ($productosCategoria as $producto) {
             echo "<li class='list-group-item'>
@@ -48,8 +55,9 @@ class ViewProducto{
         }
         echo "</ul>";
     }
-    
-    function renderCategorias($categorias){
+
+    function renderCategorias($categorias)
+    {
         echo "<ul class='list-group mt-5'>";
         foreach ($categorias as $categoria) {
             echo "<li class='list-group-item'>
