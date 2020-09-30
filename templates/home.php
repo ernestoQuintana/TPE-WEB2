c5769e2b93636015be7d760e108e39c8627b187e
Ernesto Quintana <ernesto.quintana.oliva@gmail.com>
2020-09-30 01:30:06 -0300

    agregado imagenes y video

--- templates/home.php
+++ templates/home.php
@@ -1,13 +1,49 @@
-<h1></h1>
-
-<div class="cajaHome">
-    <div class="anuncios">
-        <h2>Conoce mas de nuestros productos</h2>
-    </div>
-    <div class="anuncios">
-        <h2>Categorias de cremas</h2>
-    </div>
-    <div class="anuncios">
-        <h2>Ingresa y registrate</h2>
-    </div>
-</div>
+<main>
+    <article>
+        <section>
+            <div class="cajaHome">
+            </div>
+        </section>
+        <section class="home">
+            <h1>Nuevas Cremas Organic Shop</h1>
+            <p>Nuestras cremas son fuente natural de vitaminas y minerales, con beneficios funcionales</p>
+            <div class="propiedades">
+                <div class="icono">
+                    <img src="img\icono-cremas-eco.png" alt="" srcset="">
+                    <p>Ecologica</p>
+                </div>
+                <div class="icono">
+                    <img src="img\icono-cremas-sin-conservantes.png" alt="" srcset="">
+                    <p>Sin conservantes ni Colorantes</p>
+                </div>
+                <div class="icono">
+                    <img src="img\icono-cremas-vegetales.png" alt="" srcset="">
+                    <p>100% vegetales y Naturales</p>
+                </div>
+                <div class="icono">
+                    <img src="img\icono-cremas-vitaminas.png" alt="" srcset="">
+                    <p>Fuente Natural de Vitaminas y minerales</p>
+                </div>
+                <div class="icono">
+                    <img src="img\icono-cremas-menos-10.png" alt="" srcset="">
+                    <p>Menos de 10 Ingredientes</p>
+                </div>
+                <div class="icono">
+                    <img src="img\icono-cremas-funcionales.png" alt="" srcset="">
+                    <p>Beneficios Funcionales</p>
+                </div>
+            </div>
+            <div class="textoDescriptivo">
+                <p>
+                    La Nuevas Cremas Ecológicas Organic Shop,
+                    elaboradas con <b> Ingredientes 100% vegetales y naturales.</b>
+                    Gracias a sus ingredientes, nuestras cremas son fuente <b> natural
+                        de vitaminas y minerales,</b> con beneficios funcionales
+                </p>
+            </div>
+            <div class="videoHome">
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/IcvnZzHBOjo" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+               </div>    
+        </section>
+    </article>
+</main>
