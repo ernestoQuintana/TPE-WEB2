a9a8bf786f5b4edd2c97610c6a2fe53026558066
Ernesto Quintana <ernesto.quintana.oliva@gmail.com>
2020-10-01 22:10:44 -0300

    agregando

--- routerAvanzado.php
+++ routerAvanzado.php
@@ -15,7 +15,7 @@ $r->addRoute("allProductos", "GET", "ProductosControllers", "showProductosAdmin"
 $r->addRoute("agregarProducto", "POST", "ProductosControllers", "agregarProducto");
 $r->addRoute("eliminarProducto/:ID", "GET", "ProductosControllers", "eliminarProducto");
 $r->addRoute("editarP/:ID", "GET", "ProductosControllers", "editarP");
-$r->addRoute("editarProducto/:ID", "GET", "ProductosControllers", "editarProducto");//porque me queda en al URL EDITARP
+$r->addRoute("editarProducto/:ID", "POST", "ProductosControllers", "editarProducto");//porque me queda en al URL EDITARP
 
 //rutas admin categorias
 $r->addRoute("allCategorias", "GET", "ProductosControllers", "showCategoriasAdmin"); 
