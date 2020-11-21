0f92dc8d42ac5f85f1848e96d5b4731978eb5c22
MarcosZara <64669208+MarcosZara@users.noreply.github.com>
2020-11-21 09:06:36 -0300

    se arreglo js

--- js/comentarios.js
+++ js/comentarios.js
@@ -8,17 +8,16 @@ document.addEventListener('DOMContentLoaded', () => {
     
     document.querySelector('#formComentario').addEventListener('submit', e => {
         e.preventDefault();//evita el envio del formulario
-        agregarFila();
-        
+        agregarFila();        
     })
-
+    
     /*********************************************************************/
     /************************* TRAER COMENTARIOS *************************/
     /*********************************************************************/
-
-
-    function cargarComentarios(id_producto) {
-       
+    let id_producto = 3;
+    cargarComentarios(id_producto);
+    
+    function cargarComentarios(id_producto) {       
         div.innerHTML = "Imprimiendo...";
         fetch('api/productos/'+id_producto+'/comentarios', {
             method: "GET",
@@ -33,58 +32,15 @@ document.addEventListener('DOMContentLoaded', () => {
                 console.log(json);
                 div.innerHTML = "";
                 contenedor.innerText = "";
-
-              //   let datos = [];
-
-                 for (let i = 0; i < json.length; i++) {
-                     let id_comentario = json[i].id_comentario;
-                   // datos.push(json.comentarios[i]);
-                   // renderAdmin(json.comentarios[i]);
-                   //   let idComentario = comentario.id_comentario;
-                    let div = document.createElement('div');
-                    div.classList.add("cajaComentario");
-        
-                    let h3 = document.createElement('h3');
-                    h3.innerText = json[i].titulo;
-                    div.appendChild(h3);
-                    h3.classList.add("tituloComentario");
-        
-                    let p = document.createElement('p');
-                    p.innerText = json[i].texto + ' puntuacion: '+ json[i].puntuacion ;
-                    div.appendChild(p);
-        
-        
-                    let ul = document.createElement('ul');
-                    let li = document.createElement('li');
-                    li.appendChild(div);
-                    ul.appendChild(li);
-        
-                    let btn = document.createElement("button");
-                    btn.innerText = "Eliminar";
-                    btn.addEventListener("click", borrarComentarios(id_comentario));
-                  //  btn.setAttribute("idComentario", json[i].id_comentario);
-                    btn.classList.add("btnEliminarComentario");
-                    li.appendChild(btn);
-        
-                    ul.classList.add("listaCajasComentario")
-        
-                    contenedor.appendChild(ul);
-        
-        
-                
-
-                 }
+                renderAdmin(json);
             })
     }
-    cargarComentarios(17);
-
-
+    
     /*********************************************************************/
     /*********************** AGREGAR COMENTARIOS *************************/
     /*********************************************************************/
 
     function agregarFila() {
-
         let comentario = {
             "titulo": document.querySelector("#tituloComentario").value,
             "texto": document.querySelector("#descriptionComentario").value,
@@ -101,7 +57,7 @@ document.addEventListener('DOMContentLoaded', () => {
             return r.json()
         }).then(function (json) {
             console.log(json);
-            renderAdmin([json])
+            renderAdmin(json)
             vaciarInput();
         }).catch(function (e) {
             console.log(e)
@@ -113,47 +69,54 @@ document.addEventListener('DOMContentLoaded', () => {
         document.querySelector("#descriptionComentario").value = "";
         document.querySelector("#puntajeComentario").value = "";
     }
-
-    function renderAdmin(comentarios) {
-
-        for (let comentario of comentarios) {
-
-            //   let idComentario = comentario.id_comentario;
-            let div = document.createElement('div');
-            div.classList.add("cajaComentario");
-
-            let h3 = document.createElement('h3');
-            h3.innerText = comentario.titulo;
-            div.appendChild(h3);
-            h3.classList.add("tituloComentario");
-
-            let p = document.createElement('p');
-            p.innerText = comentario.texto + ' puntuacion: '+ comentario.puntuacion ;
-            div.appendChild(p);
-
-
-            let ul = document.createElement('ul');
-            let li = document.createElement('li');
-            li.appendChild(div);
-            ul.appendChild(li);
-
-            let btn = document.createElement("button");
-            btn.innerText = "Eliminar";
-            btn.addEventListener("click", borrarComentarios(comentario.id_comentario));
-            //btn.setAttribute("idComentario", comentarios.id_comentario);
-            btn.classList.add("btnEliminarComentario");
-            li.appendChild(btn);
-
-            ul.classList.add("listaCajasComentario")
-
-            contenedor.appendChild(ul);
-
-
-        }
+    function borrarComentarios(id_comentario,id_producto) {
+        // div.innerHTML = "Borrando...";
+        console.log('hola');
+        fetch(baseURL +'/' + id_comentario, {
+            "method": "DELETE",
+            "headers": { "Content-Type": "application/json" },
+        }).then(function(r){
+            return r.json()
+        }).then(function(json) {
+                cargarComentarios(id_producto);         
+        }).catch(function(e){
+            console.log(e);
+        })
 
     }
 
+    function renderAdmin(json) {
 
+        for (let i = 0; i < json.length; i++) {
+            let id_comentario = json[i].id_comentario;
+             let div = document.createElement('div');
+             div.classList.add("cajaComentario");
+ 
+             let h3 = document.createElement('h3');
+             h3.innerText = json[i].titulo;
+             div.appendChild(h3);
+             h3.classList.add("tituloComentario");
+ 
+             let p = document.createElement('p');
+             p.innerText = json[i].texto + ' puntuacion: '+ json[i].puntuacion ;
+             div.appendChild(p);
+
+             let ul = document.createElement('ul');
+             let li = document.createElement('li');
+             li.appendChild(div);
+             ul.appendChild(li);
+ 
+             let btn = document.createElement("button");
+             btn.innerText = "Eliminar";
+             btn.addEventListener("click" , function(){
+                 borrarComentarios(id_comentario , id_producto);
+             });        
+             btn.classList.add("btnEliminarComentario");
+             li.appendChild(btn);
+             ul.classList.add("listaCajasComentario")
+             contenedor.appendChild(ul);
+        }
+    }
     /*********************************************************************/
     /*********************** ELIMINAR COMENTARIOS ************************/
     /*********************************************************************/
@@ -161,21 +124,8 @@ document.addEventListener('DOMContentLoaded', () => {
     //     let idComentario = event.target.attributes.id_comentario.value; // esto llama al elemento que dispara el boton que cliceast
     //     borrarComentarios(idComentario);
     // }
-    function borrarComentarios(id_comentario) {
-       // div.innerHTML = "Borrando...";
-       console.log('hola');
-        fetch(baseURL +'/' + id_comentario, {
-            "method": "DELETE",
-            "headers": { "Content-Type": "application/json" },
-        }).then(response => response.json())
-            .then(function (json) {
-              
-            //   cargarComentarios(id_producto);
-            }).catch(function (e) {
-                console.log(e);
-            })
-    }
-    // autoActualizar();
+
+        // autoActualizar();
     
     // function autoActualizar(){
     //     getApi();
